5fa24fcf2bfce90811a09f2411a87ba42b37463d
shiiyan <yan39123@gmail.com>
2021-12-24 12:18:13 +0900

    refactor with validateParams

--- if_request_params.js
+++ if_request_params.js
@@ -1,4 +1,4 @@
-module.exports = class CatCollection {
+module.exports = class PetHouse {
   myCats = [
     {
       name: "tom",
@@ -18,29 +18,51 @@ module.exports = class CatCollection {
   ];
 
   search(params) {
-    let result = this.myCats;
-    if (params.name && typeof params.name === "string") {
-      result = result.filter((cat) => cat.name === params.name);
-    } else if (params.name && typeof params.name !== "string") {
-      return "params name is not valid";
-    }
-    if (params.color && typeof params.color === "string") {
-      result = result.filter((cat) => cat.color === params.color);
-    } else if (params.color && typeof params.color !== "string") {
-      return "params color is not valid";
-    }
-    if (params.age && typeof params.age === "number") {
-      result = result.filter((cat) => cat.age === params.age);
-    } else if (
-      params.age &&
-      Array.isArray(params.age) &&
-      params.age.length > 0
-    ) {
-      result = result.filter((cat) => params.age.includes(cat.age));
-    } else if (params.age) {
-      return "params age is not valid";
+    const validateResult = this.validateParams(params);
+    if (!validateResult.isValid) {
+      return validateResult.message;
     }
 
-    return result;
+    /**
+     * params
+     * name: string | undefined
+     * color: string | undefined
+     * age: [] | undefined
+     *
+     */
+    return this.myCats.filter(
+      (cat) =>
+        (params.name ? cat.name === params.name : true) &&
+        (params.color ? cat.color === params.color : true) &&
+        (params.age && params.age.length > 0 ? params.age.includes(cat.age) : true)
+    );
+  }
+
+  validateParams(params) {
+    if (params.name !== undefined && typeof params.name !== "string") {
+      return {
+        isValid: false,
+        message: "params name is not valid",
+      };
+    }
+
+    if (params.color !== undefined && typeof params.color !== "string") {
+      return {
+        isValid: false,
+        message: "params color is not valid",
+      };
+    }
+
+    if (params.age !== undefined && !Array.isArray(params.age)) {
+      return {
+        isValid: false,
+        message: "params age is not valid",
+      };
+    }
+
+    return {
+      isValid: true,
+      message: "",
+    };
   }
 };
--- test_if_request_params.js
+++ test_if_request_params.js
@@ -1,28 +1,35 @@
-const CatCollection = require("./if_request_params.js");
+const PetHouse = require("./if_request_params.js");
 
-const myCatCollection = new CatCollection();
+const myPetHouse = new PetHouse();
 
-const searchResult1 = myCatCollection.search({
+const searchResult1 = myPetHouse.search({
   color: "yellow",
 });
 
 console.log("result for 'color: yellow': ", searchResult1);
 
-const searchResult2 = myCatCollection.search({
+const searchResult2 = myPetHouse.search({
   color: "yellow",
   age: 1,
 });
 
 console.log("result for 'color: yellow, age: 1': ", searchResult2);
 
-const searchResult3 = myCatCollection.search({
+const searchResult3 = myPetHouse.search({
   age: "abc",
 });
 
 console.log("result for 'age: abc': ", searchResult3);
 
-const searchResult4 = myCatCollection.search({
+const searchResult4 = myPetHouse.search({
   age: [0, 1],
 });
 
 console.log("result for 'age: [0, 1]': ", searchResult4);
+
+const searchResult5 = myPetHouse.search({
+  name: "tom",
+  age: [1],
+});
+
+console.log("result for 'name: tom, age: [1]': ", searchResult5);
